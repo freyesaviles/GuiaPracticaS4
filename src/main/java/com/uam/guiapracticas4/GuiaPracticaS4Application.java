042bcb14c2a87f6d3c0aabee04e4d3a154f848d6
freyesaviles <freyesaviles@uamv.edu.ni>
2026-09-07 19:25:37 -0600

    Initial commit

---
+++ src/main/java/com/uam/guiapracticas4/GuiaPracticaS4Application.java
@@ -0,0 +1,13 @@
+package com.uam.guiapracticas4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaPracticaS4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiaPracticaS4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uam/guiapracticas4/GuiaPracticaS4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.guiapracticas4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaPracticaS4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
